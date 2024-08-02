dad18e25ae63544ac593cc6cd1196d098965eca8
adityakurnias <adit3407@gmail.com>
2024-08-02 19:58:54 +0700

    NEw navbar

--- src/frontend/src/component/navbar.tsx
+++ src/frontend/src/component/navbar.tsx
@@ -4,12 +4,14 @@ import Penus from "/assets/penus.png";
 
 const Navbar: React.FC = () => {
     return (
-        <nav className="w-screen fixed flex justify-between">
-        <span className="bg-white rounded-full p-auto">
-            <img src={Penus} alt="logo penus" className="w-10" />
-        </span>
-        <div>
-            <div className="clippath bg-orange-500 w-[850px] h-14"></div>
+        <nav className="w-screen fixed flex flex-col justify-between  gap-5">
+        <div className="flex justify-between border-b-[20px] my-3 border-orange-500 pb-3">
+            <span className="bg-white rounded-full flex self-center ml-16 bottom-0">
+                <img src={Penus} alt="logo penus" className="w-14" />
+            </span>
+            <div className="flex items-center justify-between relative w-screen">
+                <div className="clippath bg-orange-500 w-1/2 h-16 absolute right-0 -top-3"></div>
+            </div>
         </div>
         </nav>
     )
--- src/frontend/src/pages/login.tsx
+++ src/frontend/src/pages/login.tsx
@@ -11,18 +11,18 @@ const Login: React.FC = () => {
         <h1 className='text-slate-300 text-[64px] mb-12'>USER LOGIN</h1>
         <form className='flex flex-col gap-12 w-1/3'>
           <div className='relative flex items-center justify-center'>
-            <input type="text" placeholder="Username" className='h-14 w-[530px] p-2 rounded-full bg-[#242424] text-white text-center relative duration-700' />
-            <span className='absolute right-4 bg-slate-50 rounded-full'>
+            <input type="text" placeholder="Username" className='h-14 p-2 rounded-full bg-[#242424] text-white text-center relative duration-700 w-full' />
+            <span className='absolute -right-1 bg-slate-50 rounded-full'>
               <img src={User} className='w-12 h-12 m-2' alt='user' />
             </span>
           </div>
           <div className='relative flex justify-center items-center'>
-            <input type="password" placeholder="Password" className='h-14 w-[530px] p-2 rounded-full bg-[#242424] text-white text-center relative duration-700' />
-            <span className='absolute left-4 bg-slate-50 rounded-full'>
+            <input type="password" placeholder="Password" className='h-14 p-2 rounded-full bg-[#242424] text-white text-center relative duration-700 w-full' />
+            <span className='absolute -left-1 bg-slate-50 rounded-full'>
               <img src={Padlock} className='w-12 h-12 m-2 ' alt='padlock' />
             </span>
           </div>
-          <button type="submit" className='w-[530px] self-center p-2 rounded-full bg-white text-center font-bold text-2xl'>Login</button>
+          <button type="submit" className='w-full self-center p-2 rounded-full bg-white text-center font-bold text-2xl'>Login</button>
         </form>
       </div>
       </>
